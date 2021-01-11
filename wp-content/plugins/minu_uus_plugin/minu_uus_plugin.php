ffa7eee3f534f9a47e4605759450315dcafcc097
eensaarannemarii <anne-mari.eensaar@khk.ee>
2021-01-11 10:39:15 +0200

    modified plugin `Minu Esimene Plugin` version 1.0 (after activation of `Minu Esimene Plugin` version 1.0)

---
+++ wp-content/plugins/minu_uus_plugin/minu_uus_plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Minu Esimene Plugin
+Plugin URI: https://eensaarannemari.ikt.khk.ee/wordpress/
+Description: Minu esimene WordPressi Plugin
+Author: Anne-Mari Eensaar
+Author URI: https://eensaarannemari.ikt.khk.ee/wordpress/
+Version: 1.0
+
+
+*/
